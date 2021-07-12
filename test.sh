9f9f621bbbd40b79ce456efb0f4d8a642090841c
HarshaRani <ranishashi@gmail.com>
2021-07-13 02:52:54 +0530

    Update test.sh
    t6

--- test.sh
+++ test.sh
@@ -5,6 +5,3 @@ PYTHON=$(which python)
 
 
 $PYTHON findSim.py Curated/Jain2009_Fig3F.json --model models/synSynth7.g
-$PYTHON findSim.py Curated/Bhalla1999_fig2B.json --model models/synSynth7.g
-$PYTHON findSim.py Curated/Gu2004_fig3B.json --model models/synSynth7.g
-$PYTHON findSim.py Curated/Bhalla1999_fig4C.json --model models/synSynth7.g

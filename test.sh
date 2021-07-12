f8d4b4a343fe68a939e816c372ad193dd769f3ab
HarshaRani <ranishashi@gmail.com>
2021-07-13 02:13:23 +0530

    Update test.sh
    t6

--- test.sh
+++ test.sh
@@ -1,19 +1,9 @@
 #!/usr/bin/env bash
 
-set -e 
-set -x
-
 # virtualenv does not require --user
 PYTHON=$(which python)
 
-$PYTHON -m pip install pymoose --pre --upgrade
-$PYTHON -m pip install pylint numpy --upgrade 
-
-find . -type f -name "*.py" | xargs -I file $PYTHON -m pylint \
-    --disable=no-member \
-    --exit-zero \
-    -E file
-
+chmod +x ./scripts/moveFiles.shchmod +x ./scripts/moveFiles.shchmod +x ./scripts/moveFiles.sh
 $PYTHON findSim.py ./Curated/FindSim-Jain2009_Fig4F.tsv --model models/synSynth7.g
 $PYTHON findSim.py ./Curated/FindSim-Bhalla1999_fig2B.tsv --model models/synSynth7.g
 $PYTHON findSim.py ./Curated/FindSim-Gu2004_fig3B.tsv --model models/synSynth7.g

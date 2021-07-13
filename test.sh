b1178780eafc2e01c9405afd7ac458c9fa0ece25
HarshaRani <ranishashi@gmail.com>
2021-07-13 18:03:18 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,8 +3,7 @@
 # virtualenv does not require --user
 PYTHON=$(which python)
 
-
-$PYTHON findSim.py Curated/Jain2009_Fig3F.json --model models/synSynth7.g --map models/synSynth7_map.json
 $PYTHON findSim.py ./Curated/Bhalla1999_fig2B.json --model models/synSynth7.g --map models/synSynth7_map.json
+$PYTHON findSim.py Curated/Jain2009_Fig3F.json --model models/synSynth7.g --map models/synSynth7_map.json
 $PYTHON findSim.py ./Curated/Gu2004_fig3B.json --model models/synSynth7.g --map models/synSynth7_map.json
 $PYTHON findSim.py ./Curated/Bhalla1999_fig4C.json --model models/synSynth7.g --map models/synSynth7_map.json
